d378fcc8c520d9a37f68e1450c831f56dbe61d8e
Nurmyrza Baizhanov <104897073+Baizho@users.noreply.github.com>
2024-06-11 19:28:47 +0500

    upd

--- mind-bridge/src/components/Header.tsx
+++ mind-bridge/src/components/Header.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <div className="bg-indigo-800 h-16">
+      <div>Header</div>
+    </div>
+  );
 };
 
 export default Header;
